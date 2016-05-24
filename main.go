02283bf6342fa93dde20fd7b1cbeabce1da6e34c
igster <igor.valantic@gmail.com>
2016-05-24 21:33:05 +0200

    enabled command line argument processing

--- main.go
+++ main.go
@@ -8,32 +8,36 @@ import (
 	"os"
 	"syscall"
 	"fmt"
+	"flag"
 )
 
 func main() {
+	// command arguments processing
+	debug := flag.Bool("debug", false, "debug option uses a default home directory.")
+	flag.Parse()
 
-	var debug bool = true
+	if *debug {
+		fmt.Println("Debug mode enabled, special rules apply. ")
 
-	if debug {
 		wdBefore, _ := syscall.Getwd()
-		fmt.Println("current working directory: ", wdBefore)
+		fmt.Println("initial working directory: ", wdBefore)
 
 		// change working directory
 		syscall.Chdir("/Users/ivalantic/work/go/src/github.com/techno-chimp-www")
 
 		wdAfter, _ := syscall.Getwd()
-		fmt.Println("WD changed to: ", wdAfter )
+		fmt.Println("working directory changed to ", wdAfter )
+
+		log.Init(os.Stdout, os.Stdout, os.Stderr, "server.log")
 
 	} else {
 		syscall.Chdir("/var/www/sites/technochimp.com/public")
+		log.Init(os.Stdout, os.Stdout, os.Stderr, "/var/www/sites/technochimp.com/logs/server.log")
 	}
-
-	log.Init(os.Stdout, os.Stdout, os.Stderr, "server.log")
 
 	http.HandleFunc("/", mainComp.Handler)
 	http.HandleFunc("/githook/", gitHook.Handler)
 
 	http.ListenAndServe(":8000", log.ServerLog(http.DefaultServeMux))
-
 }
 
